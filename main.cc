10e110e647bcc0500693bdfe8d1a065638df0957
Andrej Korman <andrejkorman@gmail.com>
2025-03-09 02:15:10 +0100

    Shrink CardSet size.

--- main.cc
+++ main.cc
@@ -50,46 +50,48 @@ class Card {
 };
 
 struct CardSet {
-    CardSet()
-        : card_count_(0),
-          jokers_cnt_(0),
-          per_color_counters_(4, std::vector<int>(13, 0)) {}
+    CardSet() : jokers_cnt_(0), per_color_counters_({0, 0, 0, 0}) {}
 
     void AddCard(Card c) {
-        ++card_count_;
         if (c.is_joker()) {
             ++jokers_cnt_;
             return;
         }
-        ++per_color_counters_[static_cast<int>(c.color())][c.val()];
+        const int color_idx = static_cast<int>(c.color());
+        const uint32_t sel = Contains(c) ? 0b10 : 0b01;
+        per_color_counters_[color_idx] |= sel << (2 * c.val());
     }
     void RemoveCard(Card c) {
-        --card_count_;
-        --per_color_counters_[static_cast<int>(c.color())][c.val()];
+        const int color_idx = static_cast<int>(c.color());
+        const uint32_t sel = (Count(c) == 2) ? 0b10 : 0b01;
+        per_color_counters_[color_idx] -= sel << (2 * c.val());
     }
-    void AddJokers(int count) {
-        jokers_cnt_ += count;
-        card_count_ += count;
-    }
-    void RemoveJokers(int count) {
-        jokers_cnt_ -= count;
-        card_count_ -= count;
-    }
+    void AddJokers(int count) { jokers_cnt_ += count; }
+    void RemoveJokers(int count) { jokers_cnt_ -= count; }
 
     bool Contains(Card c) const {
-        return per_color_counters_[static_cast<int>(c.color())][c.val()] > 0;
+        const int color_idx = static_cast<int>(c.color());
+        const uint32_t sel = 0b11 << (2 * c.val());
+        return per_color_counters_[color_idx] & sel;
     }
     int Count(Card c) const {
-        return per_color_counters_[static_cast<int>(c.color())][c.val()];
+        const int color_idx = static_cast<int>(c.color());
+        const uint32_t sel = 0b11 << (2 * c.val());
+        return __builtin_popcount(per_color_counters_[color_idx] & sel);
     }
-    int size() const { return card_count_; }
+    int size() const {
+        int size = jokers_cnt_;
+        for (uint32_t x : per_color_counters_) {
+            size += __builtin_popcount(x);
+        }
+        return size;
+    }
     int empty() const { return size() == 0; }
     int jokers_count() const { return jokers_cnt_; }
 
    private:
-    int card_count_;
     int jokers_cnt_;
-    std::vector<std::vector<int> > per_color_counters_;
+    std::array<uint32_t, 4> per_color_counters_;
 };
 
 // Interesting card counts are 0 and 1. Doesn't really matter if it's 1 or 2.
